69b33fd6c3abecc4bc378aa5204e68559b00c166
Dave Hastings <150968206+DaveAnthonyHastings@users.noreply.github.com>
2025-08-16 14:34:09 -0700

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -6,16 +6,20 @@ and SQL database
 """
 import sys
 from flask import Flask
-from service import config
-from service.common import log_handlers
 from flask_talisman import Talisman
 from flask_cors import CORS
+from service import config
+from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
-talisman = Talisman(app)
-cors = CORS(app)
 app.config.from_object(config)
+
+# Add Flask Talisman
+talisman = Talisman(app)
+
+# Add Flask CORS
+cors = CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
